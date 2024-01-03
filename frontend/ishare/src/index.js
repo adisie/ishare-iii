6f4830d996f48039e0e860cb9e9db8376d0a657f
Adisie Fenta <adisiefenta23@gmail.com>
2024-01-03 15:18:44 +0300

    initial commit

---
+++ frontend/ishare/src/index.js
@@ -0,0 +1,10 @@
+import ReactDOM from 'react-dom/client'
+
+// mine
+import App from './App'
+import './index.css'
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
+  <App />
+)
